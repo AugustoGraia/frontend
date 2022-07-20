67c4e64a811f6cf6972ee12aba79fa9a50358287
Carlos Augusto Lima <81594571+AugustoGraia@users.noreply.github.com>
2022-07-20 15:22:41 -0300

    input

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,9 @@
 import Head from "next/head"
+import Image from "next/image";
+import style from '../../styles/home.module.scss';
+import {Input} from "../components/interface/input";
+
+import pizza from '../../public/pizza.png';
 
 export default function Home(){
   return(
@@ -6,8 +11,14 @@ export default function Home(){
       <Head>
         <title>GetPizzaria - Login</title>
       </Head>
-      <div>
-        <h1>Testando GetPizzaria</h1>
+      <div className={style.containerCenter}>
+        <Image className={style.img} src={pizza}/>
+        
+        <div className={style.login}> 
+          <form>
+            <Input placeholder="TESTE"/>
+          </form>
+        </div>
       </div>
     </>
   )
