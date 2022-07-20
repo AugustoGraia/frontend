b3fcc232c1aeb86af3b623eeff458de1bdac37c0
Carlos Augusto Lima <81594571+AugustoGraia@users.noreply.github.com>
2022-07-20 09:01:14 -0300

    _doc

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,14 @@
-import type { NextPage } from 'next';
+import Head from "next/head"
 
-const Home: NextPage = () => {
-  return (
-    <div>
-        <h1>Testando sass</h1>
-    </div>
+export default function Home(){
+  return(
+    <>
+      <Head>
+        <title>GetPizzaria - Login</title>
+      </Head>
+      <div>
+        <h1>Testando GetPizzaria</h1>
+      </div>
+    </>
   )
 }
-
-export default Home
