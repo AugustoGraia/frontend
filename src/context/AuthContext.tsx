02c951d52652d0794dd97a723a0f0d94659dabaf
Carlos Augusto Lima <augustolimateuto@outlook.com>
2022-07-20 22:25:39 -0300

    getform

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -30,8 +30,9 @@ export function AuthProvider({ children }: AuthProviderProps){
     const [user, setUser] = useState<UserProps>()
     const isAuthenticated = !!user;
     
-  async function signIn(){
-        alert("Clicou")
+  async function signIn({email, password}: SignInProps){
+        console.log(email)
+        console.log(password)
     }
 
     return(
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { useContext, FormEvent } from 'react';
+import { useContext, FormEvent, useState } from 'react';
 import Head from "next/head"
 import Image from "next/image";
 import styles from '../../styles/home.module.scss';
@@ -11,17 +11,21 @@ import pizza from '../../public/pizza.png';
 import { AuthContext } from '../context/AuthContext';
 
 export default function Home(){
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const [loading, setLoading] = useState(false)
+
   const { signIn } = useContext(AuthContext)
 
   async function handleLogin(event: FormEvent){
     event.preventDefault();
 
     let data = {
-      email: "teste@teste",
-      password: "1212312",
+      email,
+      password
     }
 
-    
    await signIn(data)
   }
 
@@ -38,11 +42,15 @@ export default function Home(){
             <Input 
               placeholder="Digite seu email"
               type="text"
+              value={email}
+              onChange={ (e)=> setEmail(e.target.value)}
               />       
             <Input 
               placeholder="Sua senha"
               type="password"
-              />
+              value={password}
+              onChange={ (e)=> setPassword(e.target.value)}
+              /> 
             <Button 
               type="submit"
               loading={false}
