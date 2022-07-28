9257613e8857fba385860612939b9a101d98012b
Carlos Augusto Lima <augustolimateuto@outlook.com>
2022-07-28 19:03:39 -0300

    produto

---
+++ src/pages/produto/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+import { Header } from '../../components/Header/index';
+
+export default function Produto( ) {
+    return(
+        <>
+        <Head>
+            <title>Produto - GetPizzaria</title>
+        </Head>
+            <div>
+                <Header/>
+            </div>
+        </>
+    )
+}
