36e29f4ba18e76c0c89f61a1274863501e4d6609
Carlos Augusto Lima <augustolimateuto@outlook.com>
2022-07-29 15:08:46 -0300

    upload img

--- src/pages/produto/index.tsx
+++ src/pages/produto/index.tsx
@@ -1,7 +1,30 @@
+import { ChangeEvent, useState } from 'react';
 import Head from 'next/head';
 import { Header } from '../../components/Header/index';
+import styles from './styles.module.scss';
+import { canSSRAuth } from '../../utils/canSSRAuth';
+
+import { FiUpload } from 'react-icons/fi';
 
 export default function Produto( ) {
+
+    const [avatarUrl,  setAvatarUrl] = useState('');
+    const [imagAvatar, setImageAvatar]= useState(null);
+    //Validacao para imagem
+    function handleFile(foto : ChangeEvent<HTMLInputElement>){
+        console.log(foto.target.files)
+
+        if(!foto.target.files){
+            return;
+        }
+        const imagem = foto.target.files;
+
+        if(!imagem){
+            return;
+        }
+
+    }
+
     return(
         <>
         <Head>
@@ -9,7 +32,67 @@ export default function Produto( ) {
         </Head>
             <div>
                 <Header/>
+
+                <main className={styles.container}>
+                    <h1>Novo produto</h1>
+
+                    <form className={styles.form}>
+
+                        <label className={styles.label}>
+                            <span>
+                                <FiUpload size={25} color="#fff"/>
+                            </span>
+
+                            <input type="file" accept="image/png, image/jpeg" onChange={handleFile}/>
+
+                            {avatarUrl && (
+                               <img
+                                 className={styles.preview}
+                                 src={avatarUrl} 
+                                 alt="Foto do produto" 
+                                 width={250}
+                                 height={250}
+                                />
+                            )}
+
+                        </label>
+
+                        <select>
+                            <option>
+                                Bedidas
+                            </option>
+                        </select>    
+                            <input
+                            className={styles.input}
+                            placeholder="Digite o nome do seu produto"
+                            type="text"
+                            />
+
+                            <input
+                            className={styles.input}
+                            placeholder="Preço do produto"
+                            type="text"
+                            />
+
+                            <textarea
+                            placeholder="Descreva seu produto"
+                            className={styles.input}
+                            />
+                            
+                            <button className={styles.button} type="submit">
+                                Cadastrar
+                            </button>
+
+                    </form>
+                </main>
             </div>
         </>
     )
 }
+
+export const getServerSideProps = canSSRAuth( async (cxt) => {
+
+    return{
+        props:{}
+    }
+})
