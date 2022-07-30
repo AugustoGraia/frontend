f5a4147b9c328f3434894f140a8cfd120fc996e6
Carlos Augusto Lima <augustolimateuto@outlook.com>
2022-07-30 15:05:13 -0300

    static dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,6 +1,8 @@
 import { canSSRAuth } from '../../utils/canSSRAuth';
 import Head from 'next/head'
 import {Header} from '../../components/Header/index'
+import styles from './styles.module.scss';
+import { FiRefreshCcw } from 'react-icons/fi';
 
 export default function Dashboard(){
     return(
@@ -10,7 +12,25 @@ export default function Dashboard(){
          </Head>
          <div>
             <Header />
-            
+            <main className={styles.container}>
+                <div className={styles.containerHeader}>
+                    <h1>Últimos pedidos</h1>
+                    <button>
+                        <FiRefreshCcw color="#3fffa3" size={25}/>
+                    </button>
+                </div>  
+
+                <article className={styles.listOrders}>
+
+                    <section className={styles.orderItem}>
+                        <button>
+                            <div className={styles.tag}></div>
+                            <span>ssss</span>
+                        </button>
+                    </section>
+
+                </article>
+            </main>
          </div>
        </>
     )
