139de44b66fb6ad5a701545d51b113d1c1bbc736
Carlos Augusto Lima <augustolimateuto@outlook.com>
2022-07-30 17:42:00 -0300

    modal

--- src/components/ModalOrder/index.tsx
+++ src/components/ModalOrder/index.tsx
@@ -1,8 +1,52 @@
+import Modal from 'react-modal';
+import styles from 'styles.module.scss';
 
-export function ModalOrder(){
+import { FiX } from 'react-icons/fi';
+
+import { OrderItemProps } from '../../pages/dashboard/index';
+
+interface ModalOrderProps{
+    aberto: boolean;
+    fecharModal: () => void;
+    orden: OrderItemProps[];
+
+}
+
+export function ModalOrder({ aberto, fecharModal, orden }: ModalOrderProps){
+
+    const customStyle ={
+        content:{
+            top: '50%',
+            bottom: 'auto',
+            left: '50%',
+            right: 'auto',
+            padding: '30px',
+            transform: 'translate(-50%, -50%)',
+            backgroundColor: '#1d1f2e',
+        }
+    };
+
     return(
-        <div>
-            <h1>DETALHES DO PEDIDO</h1>
-        </div>
+    <Modal
+    isOpen={aberto}
+    onRequestClose={fecharModal}
+    style={customStyle}
+    >
+
+        <button
+        type="button"
+        onClick={onRequestClose}
+        className="react-modal-close"
+        style={{ background: 'transparent', border: 0 }}
+        >
+        <FiX size={45} color="#f34748"/>
+        </button>
+
+    </Modal>
     )
 }
+
+
+
+
+
